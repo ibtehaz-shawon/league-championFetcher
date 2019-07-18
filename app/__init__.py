c15ef124bce74ab464dd3ae53fd01b446c4f540b
p1r_a_t3 <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-07-18 22:54:46 +0600

    multiprocessing implemented. buggy

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import multiprocessing
 
 from flask import Flask, Blueprint, jsonify, render_template, request
 from flask_api import status
@@ -22,19 +23,36 @@ def create_app(*args):
     @index.route('/', methods=['GET', 'POST'])
     def riot():
         valid_servers = ['euw1', 'na1', 'eun1', 'br1', 'la1', 'la2', 'tr1', 'jp1', 'kr', 'ru', 'oc1']
+        semaphore = multiprocessing.Semaphore(10)
+        
         if request.method == 'GET':
             return render_template("app/root.html", server=valid_servers)
         elif request.method == "POST":
             info = request.form
-            riot_api = info['riot_api'] if 'riot_api' in info else None
-            account_name = info['account_name'] if 'account_name' in info else None
-            server = info['servers'] if 'servers' in info else None
+            semaphore.acquire()
+            result = multiprocessing.Manager().dict()
+            background_job = multiprocessing.Process(target=form_data_manipulation,
+                                                    args=(info, semaphore, result))                                                                                                                                                            
+            background_job.start()
+            background_job.join()
+            print(dict(result))
+            print("LEL")
+            return render_template("app/root.html", result=dict(result), server=valid_servers)
 
-            riot = Riot(account_name, server, riot_api)
-            result = riot.master_controller()
-            return render_template("app/root.html",
-                                   result=result,
-                                   server=valid_servers)
+    
+    def form_data_manipulation(info, semaphore, result):
+        """
+        manipulates the form data here.
+        """
+        riot_api = info['riot_api'] if 'riot_api' in info else None
+        account_name = info['account_name'] if 'account_name' in info else None
+        server = info['servers'] if 'servers' in info else None
+
+        riot = Riot(account_name, server, riot_api)
+        result = riot.master_controller()
+        print(dict(result))
+        print("L00000L")
+        semaphore.release()
 
     @app.route("/")
     def root():
